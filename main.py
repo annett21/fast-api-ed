315cd6386575955b4ff916e33db5327e045c175b
annett21 <annettarasevich@gmail.com>
2023-04-17 17:17:28 +0300

    added query

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from enum import Enum
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
+from typing import Annotated
 
 
 class ModelName(str, Enum):
@@ -90,3 +91,9 @@ async def create_item(item: Item):
         price_with_tax = item.price + item.tax
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
+
+
+@app.get("/items/")
+async def read_items(q: Annotated[list[str] | None, Query()] = None):
+    query_items = {"q": q}
+    return query_items
