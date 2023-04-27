3ab998928bd93a0cad30117275ee76011518e2f1
annett21 <annettarasevich@gmail.com>
2023-04-27 16:29:12 +0300

    added depends class

--- main_four.py
+++ main_four.py
@@ -11,11 +11,35 @@ async def common_parameters(
     return {"q": q, "skip": skip, "limit": limit}
 
 
-@app.get("/items/")
-async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
-    return commons
+# @app.get("/items/")
+# async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
+#     return commons
 
 
 @app.get("/users/")
 async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
     return commons
+
+
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"},
+]
+
+
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/items/")
+async def read_items(commons: Annotated[CommonQueryParams, Depends()]):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
